3cd7f21fb303f2b55d9949016d595f1e570f9e30
AaronHchen <55475471+Aaronhchen@users.noreply.github.com>
2020-04-28 13:41:30 -0700

    Add files via upload
    Updated HTML files from Steven & Boostrap Navbar from Nolan

---
+++ app.py
@@ -0,0 +1,122 @@
+from flask import Flask, render_template, redirect, url_for, request, session, g, jsonify
+from flask_nav import Nav
+from flask_nav.elements import Navbar, View, Subgroup, Separator, Link
+from flask_bootstrap import Bootstrap
+from peewee import SqliteDatabase
+from database import User, Cards
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+import bcrypt  # for hashing
+
+app = Flask(__name__)
+Bootstrap(app)
+nav = Nav()
+
+app.secret_key = 'SuperSecretKeyForAgileProject'
+db = SqliteDatabase('clicker.sqlite')
+# init for managing logins
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@nav.navigation()
+def mynavbar():
+    if current_user.is_authenticated:
+        return Navbar(
+            'Menu',
+            View('Home', 'home'),
+            View(f'Logout ({g.user.username})', 'logout'),
+            View('Profile', 'profile')
+        )
+
+
+@app.before_request
+def get_db():
+    """ Initialize connection with database"""
+    db.connect()
+
+
+@app.before_request
+def before_request():
+    """ For sessions """
+    g.user = None
+    if 'user_id' in session:
+        user = User.get(User.id == session['user_id'])
+        g.user = user
+
+
+@app.teardown_request
+def close_connection(exception):
+    """ For closing database when not in use """
+    if not db.is_closed():
+        db.close()
+
+
+@app.route('/home', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    """ Required for managing user login """
+    return User.get(user_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """ Logs User in """
+    error = None
+    if request.method == 'POST':
+        session.pop('user_id', None) # reset session
+        username = request.form["username"]
+        passwd = request.form["password"]
+        loginUser = User.get(User.username == username)
+        if loginUser and bcrypt.checkpw(passwd.encode("utf-8"), loginUser.password.encode("utf-8")):
+            # If hashed passwwords match, then generate session and login user
+            print(loginUser.password)
+            session["user_id"] = User.get(User.username == username).id
+            login_user(loginUser)
+            return redirect(url_for('profile'))
+        else:
+            error = "Invalid Login Credentials"
+            return render_template('login.html', error=error)
+    else:
+        return render_template('login.html', error=error)
+
+@app.route('/profile')
+@login_required
+def profile():
+    """ Displays User Profile """
+    if not g.user:  # Checks user stored in session (not sure this is actually needed anymore with login manager)
+        return redirect(url_for('login', error="unauthorized"))
+    return render_template('profile.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    """ Registers a new user and hashes password, also logs in after """
+    error = None
+    if request.method == 'POST':
+        username = request.form["new_username"]
+        password = request.form["new_password"].encode("utf-8")
+        hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+        newuser = User.create(username=username, password=hashed)
+        session["user_id"] = User.get(User.username == newuser.username).id
+        login_user(newuser)
+        return redirect(url_for('profile'))
+    if request.method == 'GET':
+        return render_template('register.html', error=error)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    """ Logs user out when called """
+    logout_user()
+    return redirect(url_for('login'))
+
+
+if __name__ == '__main__':
+    nav.init_app(app)
+    app.run(debug=True)
---
+++ database.py
@@ -0,0 +1,33 @@
+from peewee import SqliteDatabase, Model, CharField, IntegerField, ForeignKeyField
+from flask_login import UserMixin
+
+db = SqliteDatabase('clicker.sqlite')
+db.connect()
+
+
+class User(UserMixin, Model):
+    """ Peewee model for Abstract Unit """
+    username = CharField(unique=True)
+    password = CharField()
+
+    def __str__(self): return self.username
+
+    class Meta:
+        database = db
+
+
+class Cards(Model):
+    """ Peewee model for Hero Unit """
+    card_name = CharField(unique=True)
+    rarity_value = CharField()
+
+    class Meta:
+        database = db
+
+    def __str__(self): return self.card_name
+
+if __name__ == "__main__":
+    db.drop_tables([User, Cards])
+    db.create_tables([User, Cards])
+    user = User.create(username='test', password='password')
+
