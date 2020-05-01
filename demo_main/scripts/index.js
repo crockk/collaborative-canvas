d68aea7d8390e3317996a6b4b6180f5d18c1abf4
Steven <zzt2143@hotmail.com>
2020-04-30 19:11:14 -0700

    create demo of main page

---
+++ demo_main/scripts/index.js
@@ -0,0 +1,60 @@
+let drawarea=document.getElementById("myCanvas");
+
+// let img=new Image();
+// let context = drawarea.getContext('2d');
+// context.drawImage(img,0,0);
+// context.fillStyle = '#05EFFF';
+// context.fillRect(0, 0, 150, 100);
+
+$(document).ready(function(){
+    let rows=30;
+    let cols=30;
+    for(i=0; i<rows; i++){
+        drawarea.innerHTML+="<tr class='table_row'></tr>";
+    
+    }
+    for(j=0; j<cols; j++){
+        $('tr').append("<td class='cell'></td>");
+
+    }
+    
+    const cells = document.querySelectorAll('.cell');
+    for (var i = 0; i < cells.length; i++) {
+        cells[i].addEventListener('click', function (event) {    
+        const color = "black";
+        event.target.style.backgroundColor = color;
+        });
+    }
+});
+
+
+// draw on canvas
+// function drawing(event){
+//     let pageW=parseInt(document.body.clientWidth);
+//     let posx=event.pageX-((pageW-500)/2);
+//     let posy=event.pageY-97;
+//     // console.log(`${posx},${posy}`);
+//     // console.log(pageW);
+    
+//     context.fillStyle = '#05EFFF';
+//     context.fillRect(posx*0.6, posy*0.37, 5, 5);
+
+//     //img url - need to send to python and decode there
+//     let dataUrl = drawarea.toDataURL();
+//     console.log(dataUrl);
+
+    
+
+// }
+// drawarea.addEventListener("click",drawing);
+
+
+
+
+
+
+
+
+
+
+
