96aa7027b60e971b4fa450e27c3e1058f3e29673
crockk <58015900+crockk@users.noreply.github.com>
2020-05-21 17:32:12 -0700

    Fixed importerrors for card

--- database.py
+++ database.py
@@ -29,8 +29,8 @@ class Pixels(Model):
 
 
 if __name__ == "__main__":
-    db.drop_tables([User, Cards, Pixels])
-    db.create_tables([User, Cards, Pixels])
+    db.drop_tables([User, Pixels])
+    db.create_tables([User, Pixels])
     user = User.create(username='test', password='password')
     home = User.create(username='home', password='none')
 
